9dca8eb2b5ac6c87489fc9196c8cef8e6f8b3581
Apekshit <apekshit33@gmail.com>
2015-01-26 20:41:00 +0530

    Integrated Google BigQuery

--- app.py
+++ app.py
@@ -1,5 +1,15 @@
+import httplib2
 import webapp2
 from google.appengine.ext.webapp import template
+from apiclient.discovery import build
+from oauth2client.appengine import AppAssertionCredentials
+
+url = 'https://www.googleapis.com/auth/bigquery'
+PROJECT_NUMBER = '803808993161'
+
+credentials = AppAssertionCredentials(scope=url)
+httpss = credentials.authorize(httplib2.Http())
+bigquery_service = build('bigquery','v2',http=httpss)
 
 class ShowHome(webapp2.RequestHandler):
 	def get(self):
@@ -11,7 +21,11 @@ class ShowChartPage(webapp2.RequestHandler):
 	def get(self):
 		temp_data = {}
 		temp_path = 'Templates/chart.html'
-		self.response.out.write(template.render(temp_path,temp_data))
+		queryData = {'query':'SELECT word FROM [publicdata:samples.shakespeare] LIMIT 1000'}
+		tableData = bigquery_service.jobs()
+		response = tableData.query(projectId=PROJECT_NUMBER,body=queryData).execute()
+		self.response.out.write(response)
+		#self.response.out.write(template.render(temp_path,temp_data))
 		
 application = webapp2.WSGIApplication([
 	('/chart', ShowChartPage),
